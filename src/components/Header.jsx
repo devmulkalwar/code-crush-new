c70836da1dd57c33cd98e91e9231194fbca4306f
devmulkalwar <devmulkalwar95@gmail.com>
2024-10-23 11:30:17 +0530

    merging to main branch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,45 +5,49 @@ import logo from "../assets/images/logo.png";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-    const [showDropdown, setShowDropdown] = useState(false);
-    const { user, uid, setUid, setActive } = useGlobalContext();
+  const [showDropdown, setShowDropdown] = useState(false);
+  const { user, uid, setUid, setActive } = useGlobalContext();
 
-    const handleImageClick = () => {
-        setShowDropdown((p) => !p);
-    };
+  const handleImageClick = () => {
+    setShowDropdown((p) => !p);
+  };
 
-    const currentRoute = window.location.pathname.substring(1);
-    useEffect(() => {
-        if (currentRoute === "") {
-            setActive(0);
-        }
-    }, [currentRoute]);
+  const currentRoute = window.location.pathname.substring(1);
+  useEffect(() => {
+    if (currentRoute === "") {
+      setActive(0);
+    }
+  }, [currentRoute]);
 
-    return (
-        <header className="header">
-            <div className="header-logo">
-                <Link to="/" onClick={() => setActive(0)}>
-                    <img className="logo-img" src={logo} alt="logo" />
-                    <span>FinGo</span>
-                </Link>
-            </div>
+  return (
+    <header className="header">
+      <div className="header-logo">
+        <Link to="/" onClick={() => setActive(0)}>
+          <img className="logo-img" src={logo} alt="logo" />
+          <span>FinGo</span>
+        </Link>
+      </div>
 
-            <Link to="/profile" onClick={() => setActive(-1)}>
-                <div
-                    className="header-img"
-                >
-                    {uid === null ? (
-                        <img
-                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX5DWWYRWd7uysUpQK690_mjjaBPgll2-V0Q&usqp=CAU"
-                            alt="profile"
-                        />
-                    ) : (
-                        <img src={`${user?.photoURL}`} alt="profile" />
-                    )}
-                </div>
-            </Link>
-        </header>
-    );
+      <Link to="/profile" onClick={() => setActive(-1)}>
+        <div className="header-img">
+          {uid === null ? (
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX5DWWYRWd7uysUpQK690_mjjaBPgll2-V0Q&usqp=CAU"
+              alt="profile"
+            />
+          ) : (
+            <img
+              src={
+                user?.photoURL ||
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX5DWWYRWd7uysUpQK690_mjjaBPgll2-V0Q&usqp=CAU"
+              }
+              alt="profile"
+            />
+          )}
+        </div>
+      </Link>
+    </header>
+  );
 };
 
 export default Header;
